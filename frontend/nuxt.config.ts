9abab249c7f46090a01ef7453df10335ea0860d6
Bruno Bastos <pinheirobastos@gmail.com>
2023-09-11 12:43:34 -0300

    feat: add tailwind + daisyui

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],  
+  runtimeConfig: {
+    NUXT_SECRET: process.env.NUXT_SECRET,
+    BACK4APP_URL: process.env.BACK4APP_URL,
+    BACK4APP_APPID: process.env.BACK4APP_APPID,
+    BACK4APP_RESTAPIKEY: process.env.BACK4APP_RESTAPIKEY,
+  }
 })
