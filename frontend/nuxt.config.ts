db4465e8494ba83cb3b4e9e04d5919e21131032e
Bruno Bastos <pinheirobastos@gmail.com>
2023-09-11 16:37:50 -0300

    feat: adicionando pinia

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@tailvue/nuxt'
   ],  
   runtimeConfig: {
-    NUXT_SECRET: process.env.NUXT_SECRET,
-    BACK4APP_URL: process.env.BACK4APP_URL,
-    BACK4APP_APPID: process.env.BACK4APP_APPID,
-    BACK4APP_RESTAPIKEY: process.env.BACK4APP_RESTAPIKEY,
-  }
+    public: {
+      NUXT_SECRET: process.env.NUXT_SECRET,
+      BACK4APP_URL: process.env.BACK4APP_URL,
+      BACK4APP_APPID: process.env.BACK4APP_APPID,
+      BACK4APP_RESTAPIKEY: process.env.BACK4APP_RESTAPIKEY
+    }
+  },
 })
