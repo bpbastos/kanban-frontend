186101eabe82c5e786dc0b1afa7dbf6f07e6b40f
Bruno Bastos <bruno.bastos@iag.puc-rio.br>
2023-09-14 17:29:25 -0300

    feat: priorities query

--- kanban-data/schema/query.py
+++ kanban-data/schema/query.py
@@ -3,10 +3,12 @@ from sqlalchemy import select
 import strawberry
 
 from .board import Board
+from .priority import Priority
 from . import Info
 from . import UserNotFound
 
 from models.board import Board as BoardModel
+from models.priority import Priority as PriorityModel
 from models import get_session
 
 @strawberry.type
@@ -21,6 +23,17 @@ class Query:
             sql = select(BoardModel).filter(BoardModel.user_id == user_id)
             db_boards = (await s.execute(sql)).scalars().unique().all()
         return [Board.marshal(board) for board in db_boards]
+
+    @strawberry.field()
+    async def priorities(self, info: Info) -> list[Priority]:
+        user_id = info.context.user.get('id')
+        #if not user_id:
+        #    return UserNotFound()
+        
+        async with get_session() as s:
+            sql = select(PriorityModel).filter(PriorityModel.user_id == user_id)
+            db_priorities = (await s.execute(sql)).scalars().unique().all()
+        return [Priority.marshal(priority) for priority in db_priorities]    
 
     @strawberry.field()
     async def board(self, info: Info, id: strawberry.ID) -> Board:
