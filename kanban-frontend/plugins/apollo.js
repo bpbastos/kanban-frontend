d87984aba2de5b3e460d8bfbb467f49e918ee9c8
Bruno Bastos <bruno.bastos@iag.puc-rio.br>
2023-09-18 18:30:10 -0300

    feat: plugin para definição de cabeçalhos customizados nas querys/mutations graphql

---
+++ kanban-frontend/plugins/apollo.js
@@ -0,0 +1,38 @@
+import { useUserStore } from "@/store/user";
+import { createHttpLink, from, InMemoryCache, ApolloClient } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context'
+import { provideApolloClient } from '@vue/apollo-composable'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+  const { user } = useUserStore();
+
+  const httpLink = createHttpLink({
+    uri: config.public.KANBANDATA_URL,
+  });
+
+  // authentication headers
+  const authLink = setContext(async (_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        'X-User-Id': 's8OfY9OBAE'/*user.id* testando ...*/
+      },
+    };
+  });
+
+  console.log(user.id)
+
+  // Cache implementation
+  const cache = new InMemoryCache();
+
+  // Create the apollo client
+  const apolloClient = new ApolloClient({
+    link: from([authLink, httpLink]),
+    cache,
+  });
+
+  provideApolloClient(apolloClient);
+
+  nuxtApp._apolloClients.default = apolloClient
+});
