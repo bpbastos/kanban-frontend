6df7a34feb2d260a3cd9f57a32febc9582c26672
Bruno Bastos <bruno.bastos@iag.puc-rio.br>
2023-09-18 10:38:09 -0300

    fix: pequenos ajustes no frontend

--- kanban-frontend/layouts/default.vue
+++ kanban-frontend/layouts/default.vue
@@ -1,18 +1,13 @@
 <template>
-    <div class="container mx-auto max-w-screen-xl max-h-screen ">
-      <PageHeader 
-        username="yoda"
-        firstName="Yoda"
-        lastName="Minch"
-        profilePicture="https://avatars.githubusercontent.com/u/1862395?v=4"
-      />
-      <div class="flex shadow-2xl">
-        <SideBar />
-        <main class="py-8 px-8 w-screen">
-          <div class="flex flex-col">
-            <slot />
-          </div>
-        </main>
-      </div>
+  <div class="container mx-auto max-w-screen-xl max-h-screen ">
+    <PageHeader username="yoda" firstName="Yoda" lastName="Minch" profilePicture="/images/avatar.jpg" />
+    <div class="flex shadow-2xl">
+      <SideBar />
+      <main class="py-8 px-8 w-screen">
+        <div class="flex flex-col">
+          <slot />
+        </div>
+      </main>
     </div>
+  </div>
 </template>
