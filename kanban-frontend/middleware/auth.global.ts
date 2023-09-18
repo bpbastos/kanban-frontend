9a0447f81a730275687fa5a209472bf76d03ac73
Bruno Bastos <bruno.bastos@iag.puc-rio.br>
2023-09-18 16:12:31 -0300

    refactor: refatoração no código que checa a autenticação

--- kanban-frontend/middleware/auth.global.ts
+++ kanban-frontend/middleware/auth.global.ts
@@ -1,22 +1,13 @@
-import { storeToRefs } from 'pinia';
 import { useUserStore } from '~/store/user';
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    const { setAuthenticated } = useUserStore(); 
-    const token = useCookie('token'); // get token from cookies
-  
-    if (token.value) {
-      // check if value exists
-      setAuthenticated(true); // update the state to authenticated
-    }
-  
-    // if token exists and url is /signin or /sign redirect to homepage
-    if ((token.value && to?.name === 'signin') || (token.value && to?.name === 'signup')) {
+    const { user } = useUserStore(); 
+    
+    if ((user.authenticated && to?.name === 'signin') || (user.authenticated && to?.name === 'signup')) {
       return navigateTo('/board');
     }
 
-    // if token doesn't exist redirect to log in
-    if (!token.value && to?.name !== 'signin' && !token.value && to?.name !== 'signup') {
+    if (!user.authenticated && to?.name !== 'signin' && !user.authenticated && to?.name !== 'signup') {
       abortNavigation();
       return navigateTo('/signin');
     }
