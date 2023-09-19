7724655cfeb229a82ad4c275d13ba3c1d823eafc
Bruno Bastos <bruno.bastos@iag.puc-rio.br>
2023-09-19 15:36:10 -0300

    feat: função para adicionar um novo quadro

---
+++ kanban-frontend/components/AddNewBoard.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="flex flex-row w-full gap-1" v-show="show">
+        <div class="mr-3 w-full">
+            <input type="text" placeholder="Insira um nome para o quadro..." class="input input-primary input-md w-full"
+                maxlength="45" v-model="newBoardName" tabindex="0" ref="newBoardInput" />
+        </div>
+        <button class="btn btn-success" @click="addNewBoard">
+            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+            </svg>
+        </button>
+        <button class="btn btn-error " @click="emit('canceled')">
+            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+
+    </div>
+</template>
+  
+<script setup>
+const newBoardName = ref('')
+const newBoardInput = ref(null)
+
+const CREATE_BOARD_MUTATION = gql`
+mutation addNewBoard ($name: String!) {
+    addBoard(
+        board: {
+            name: $name
+            workflows:[
+                {
+                    color: "purple"
+                    name: "Backlog"
+                },
+                {
+                    color: "orange"
+                    name: "Em andamento"
+                },
+                {
+                    color: "blue"
+                    name: "Em análise"
+                },
+                {
+                    color: "green"
+                    name: "Concluído"
+                }
+            ]            
+        }    
+    )
+    {
+        id
+    }
+} 
+`
+const { mutate: addNewBoardMutation } = useMutation(CREATE_BOARD_MUTATION);    
+
+const props = defineProps({
+    show: Boolean
+});
+
+const emit = defineEmits(['added', 'canceled'])
+
+const addNewBoard = async() => {
+  if (newBoardName.value.trim()) {
+    const res = await addNewBoardMutation({ 
+      name: newBoardName.value.trim()
+    })
+    if (res) {
+      emit('added', res.data.id)
+      newBoardName.value = ""   
+    } 
+  }    
+}
+
+//Focus the input as soon as DOM el is shown
+onUpdated(() => {
+    nextTick(() => {
+        newBoardInput.value.focus()
+    });
+})
+</script>
--- kanban-frontend/pages/board.vue
+++ kanban-frontend/pages/board.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <div class="pb-8">
-      <BoardSwitcher />
+    <div class="flex items-center gap-2">
+      <BoardSwitcher :key="boardSwitcherUpdate"/>
+      <div class="flex flex-row w-4/12">
+    
+        <AddNewBoard :show="showAddBoardForm" @canceled="showAddBoardForm = false" @added="addedNewBoard" />
+
+        <button class="btn btn-primary" @click.stop="showAddBoardForm = !showAddBoardForm" v-if="!showAddBoardForm">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+        </button>
+      </div>
     </div>
+
   </div>
 </template>
 <script setup>
-import { storeToRefs } from 'pinia'
-import { useUserStore } from '@/store/user';
 
-const { user } = storeToRefs(useUserStore())
+const showAddBoardForm = ref(false)
+const boardSwitcherUpdate = ref(0)
+
+const addedNewBoard = () => {
+  boardSwitcherUpdate.value++;
+  showAddBoardForm.value = false;
+}
 
 definePageMeta({
-    layout: "default",
+  layout: "default",
 });
 </script>
