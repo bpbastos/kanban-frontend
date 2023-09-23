94992777a547a47d7896b8399ef4c62f81ad924a
Bruno Bastos <bruno@bastoss.com>
2023-09-23 17:28:05 -0300

    build: atualização no entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
-APP_PATH=/app
-echo > .env
-echo BACK4APP_URL=$BACK4APP_URL >>.env
-echo BACK4APP_APPID=$BACK4APP_APPID >> .env
-echo BACK4APP_RESTAPIKEY=$BACK4APP_RESTAPIKEY >> .env
-echo KANBANDATA_URL=$KANBANDATA_URL >> .env
-echo NUXT_SECRET=$NUXT_SECRET >> .env
-
-sleep 5
 
 export $(cat .env | egrep -v '#|^$' | xargs) && node .output/server/index.mjs
